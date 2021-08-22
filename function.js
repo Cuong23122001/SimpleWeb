ee3217ffa0bd59b8957fecb0d56733f51ad0257a
Cuong23122001 <cuongnmgch190696@fpt.edu.vn>
2021-08-22 13:04:01 +0700

    ok

--- function.js
+++ function.js
@@ -10,22 +10,22 @@ async function getDB() {
 
 async function InserPet(newPet){
     const db = await getDB();
-    await db.collection("pets").insertOne(newPet);
+    await db.collection("toys").insertOne(newPet);
 }
 async function DeletePet(id){
     const db = await getDB();
-    await db.collection("pets").deleteOne({_id:ObjectId(id)});
+    await db.collection("toys").deleteOne({_id:ObjectId(id)});
 }
 async function UpdatePet(id, nameInput,imageInput,priceInput) {
     const filter = { _id: ObjectId(id) };
     const newValue = { $set: { name: nameInput,image: imageInput,price:priceInput } };
 
     const dbo = await getDB();
-    await dbo.collection("pets").updateOne(filter, newValue);
+    await dbo.collection("toys").updateOne(filter, newValue);
 }
 async function getPetID(id) {
     const dbo = await getDB();
-    const e = await dbo.collection("pets").findOne({ _id: ObjectId(id) });
+    const e = await dbo.collection("toys").findOne({ _id: ObjectId(id) });
     return e;
 }
 module.exports = {getDB,InserPet,DeletePet,getPetID,UpdatePet};
